5ccbecffee892e7e7f76ed2bb25c6cfecd583278
VIVEKSUREKA <53266779+VIVEKSUREKA@users.noreply.github.com>
2023-05-01 02:38:18 +0530

    Update index.js

--- MERN-Web-App/client/src/api/index.js
+++ MERN-Web-App/client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL : 'http://localhost:5000'});
+// const API = axios.create({baseURL : 'http://localhost:5000'});
 
-// const API = axios.create({baseURL : 'https://remeniscing-memories.herokuapp.com/'});
+const API = axios.create({baseURL : 'https://memories-assemble.onrender.com/'});
 
 // const url = 'https://reminiscing-anecdotes.herokuapp.com/posts';
 
